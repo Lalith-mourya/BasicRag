6c7f8c6a5d2a2e03f47858c9db8a3e5569661269
lalith23bce8550 <lalith.23bce8550@vitapstudent.ac.in>
2026-06-24 03:17:05 +0530

    Done changes with adding retriever chain and document chain

--- app.py
+++ app.py
@@ -3,27 +3,57 @@ load_dotenv()
 import os
 
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
-os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain 
+from langchain_core.prompts import ChatPromptTemplate , MessagesPlaceholder
+from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
 
 st.title("MONGODB ChatBot")
+
  ## LLM 
-from langchain_groq import ChatGroq
-llm = ChatGroq(model="llama-3.1-8b-instant")
+llm = ChatGroq(model="deepseek-r1-distill-llama-70b")
 
 ## prompt
-from langchain_core.prompts import ChatPromptTemplate , MessagesPlaceholder
-prompt = ChatPromptTemplate.from_messages([
-    ("system","Think that you are a helpfull assistant and answer to the following questions"),
-    #MessagesPlaceholder(variable_name="history"),
-    ("human","{question}")
-])
+prompt = ChatPromptTemplate.from_template(
+    
+    """
+   You are a MongoDB notes assistant.
+
+Use ONLY the information present in the provided context.
+
+Rules:
+1. Do not use outside knowledge.
+2. Do not add explanations that are not present in the context.
+3. If asked for topics, return only the topic names found in the context.
+4. If the answer is not found in the context, say:
+   "This information is not available in the notes."
+5. Do not infer, expand, or generate additional MongoDB content.
+
+
+Context:
+{context}
+
+    Question: {input}
+    """
+)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorstore = Chroma(
+    persist_directory="./vector_db",
+    embedding_function=embeddings
+)
+
+retriever = vectorstore.as_retriever()
 
 # parser
 parser = StrOutputParser()
@@ -31,7 +61,17 @@ parser = StrOutputParser()
 question = st.text_input("Enter your question")
 if question:
 
-    chain = prompt | llm | parser
+    document_chain = create_stuff_documents_chain(llm, prompt)    
+    retriever_chain = create_retrieval_chain(retriever,document_chain)
+    docs = retriever.invoke(question)
 
-    result = chain.invoke(question)
-    st.write(result)
+    for i, doc in enumerate(docs):
+        st.write(f"\nChunk {i+1}")
+        st.write(doc.page_content[:1000])
+    result = retriever_chain.invoke({"input":question})
+
+    st.write("CONTEXT SENT TO LLM")
+    st.write(result["context"])
+
+    st.write("ANSWER")
+    st.write(result["answer"])
